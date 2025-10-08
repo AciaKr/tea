7274c6c1c090b8797eb9af80dac6c4360aace34c
Nastassia Krasutskaya <krasnastassia@gmail.com>
2025-10-08 14:16:29 +0300

    add routing, release popup on main page

--- src/app/components/pages/main/main.component.ts
+++ src/app/components/pages/main/main.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Subject, takeUntil, timer} from "rxjs";
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
+  @ViewChild('promoPopup') popupElement!: ElementRef;
+  private popupInstance?: Modal;
+  private observable$: Subject<void> = new Subject<void>();
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit(): void {
+    const shown: string | null = sessionStorage.getItem('popupShown');
+
+    if (!shown) {
+      timer(10000)
+        .pipe(takeUntil(this.observable$))
+        .subscribe((): void => {
+          this.showPopup();
+          sessionStorage.setItem('popupShown', 'true');
+        });
+    }
   }
 
+  showPopup(): void {
+    if (this.popupElement) {
+      this.popupInstance = new Modal(this.popupElement.nativeElement);
+      this.popupInstance.show();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.observable$.next();
+    this.observable$.complete();
+    this.popupInstance?.hide();
+  }
 }
